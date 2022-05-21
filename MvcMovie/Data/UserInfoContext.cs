ac0909191a137677fac14de82fd6d03375da3144
Echo-Zhou7 <1583512140@qq.com>
2022-05-21 17:24:48 +0800

    111

---
+++ MvcMovie/Data/UserInfoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class UserInfoContext : DbContext
+    {
+        public UserInfoContext (DbContextOptions<UserInfoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.UserInfo>? UserInfo { get; set; }
+    }
+}
---
+++ MvcMovie/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcMovie.Models
+{
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string InterestLabel { get; set; }   
+        public string Picture { get; set; }
+
+
+    }
+}
